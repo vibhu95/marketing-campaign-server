e5d351f036f70149a8290754f147690670e88466
vibhuti1 <vibhuti@antrepriz.com>
2020-06-12 01:52:23 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // const cluster = require('cluster');
 // const numCPUs = require('os').cpus().length;
-// const Routes = require('./routers/Routes');
+const Routes = require('./routers/Routes');
 // const morgan = require('morgan');
 const G = require('./globals');
-// const _init = require('./controllers/init');
+const _init = require('./controllers/init');
 // const fs = require('fs');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 
@@ -15,20 +15,20 @@ const app = express();
 // log all requests to error.log
 // app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, 'error.log'), { flags: 'a' }) }))
 
-// app.use(_setCors);
+app.use(_setCors);
 
-// const routers = new Routes(app);
-// routers.init_Routes();
-// _init();
+const routers = new Routes(app);
+routers.init_Routes();
+_init();
 G.rootPath = __dirname;
 
 app.listen(process.env.PORT || 7800, function() {
   console.log("Server up and listening");
 });
 
-// function _setCors(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", ['*'].join());
-//   res.header("Access-Control-Allow-Methods", ['GET','PUT','POST','OPTIONS'].join());
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// }
+function _setCors(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ['*'].join());
+  res.header("Access-Control-Allow-Methods", ['GET','PUT','POST','OPTIONS'].join());
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+}
