af8a1861476bb3ec43a844141aea325d706286cd
vibhuti1 <vibhuti@antrepriz.com>
2020-06-12 15:39:14 +0530

    changes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 // const cluster = require('cluster');
 // const numCPUs = require('os').cpus().length;
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const G = require('./globals');
 const _init = require('./controllers/init');
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
 // filter out successfull responses for morgan
-// app.use(morgan('dev', { skip : (req, res) => res.statusCode < 400 }));
+app.use(morgan('dev', { skip : (req, res) => res.statusCode < 400 }));
 // log all requests to error.log
-// app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, 'error.log'), { flags: 'a' }) }))
+app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, 'error.log'), { flags: 'a' }) }))
 
 app.use(_setCors);
 
@@ -22,6 +22,8 @@ _init(app);
 app.listen(process.env.PORT || 7800, function() {
   console.log("Server up and listening");
 });
+
+console.log("SENDGRID_API_KEY ::: ",process.env.SENDGRID_API_KEY);
 
 function _setCors(req, res, next) {
   res.header("Access-Control-Allow-Origin", ['*'].join());
